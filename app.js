6463d8ca216fe084b1ddc31c660b006fd915ce17
fangbrian <fangbrian@gmail.com>
2014-11-22 18:11:07 -0800

    cleaned up the code a bit

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var http = require("http");
-var url = require("url");
 
-var alink = 'http://google.com';
-var mapping = {key:'1', site:alink};
+//Globals
 var MAX_MEMORY_ENTRIES = 10;
 var memoryKey = new Array(MAX_MEMORY_ENTRIES);
 var memoryLink = new Array(MAX_MEMORY_ENTRIES);
@@ -34,7 +31,6 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:name', function(req, res) {
-	console.log('Input: ' + req.params.name);
 	//1. Loop through array to find if key exists
 	var i;
 	for(i = 0; i < currMemory; i++) {	
@@ -50,12 +46,7 @@ app.get('/:name', function(req, res) {
 });
 
 app.post('/myaction', function(req, res) {
-//Get the hyperlink
-//Get the key
-//Save to data structure 
-//Save to array
 
-//TODO: CHECK FOR VALID ENTRY (enough memory, no duplicate keys)
 	var link = req.body.link;
 	var linkKey = req.body.userkey;
 	
@@ -69,10 +60,9 @@ app.post('/myaction', function(req, res) {
 		link = "www." + link;
 	}
 	
-	//Check to see if we have enough memory
+	//Check to see if we have enough memory or if the key is valid
 	if((currMemory < MAX_MEMORY_ENTRIES) && (checkValidKey(linkKey) === MAX_MEMORY_ENTRIES)) {
 		memoryKey[currMemory] = linkKey;
-		//TODO: check to see if there is a http:// in front of it 
 		memoryLink[currMemory] = 'http://' + link;
 		var shortenedUrl = "http://localhost/" + memoryKey[currMemory];
 		res.json(shortenedUrl);
@@ -81,7 +71,7 @@ app.post('/myaction', function(req, res) {
 	else {
 		//Error local memory full
 		if(currMemory >= MAX_MEMORY_ENTRIES){
-			res.json("Error: Invalid Entry. Not enough local memory. Current capacity: " + MAX_MEMORY_ENTRIES);
+			res.json("Error: Not enough local memory. Current capacity: " + MAX_MEMORY_ENTRIES);
 		}
 		//Error key already exists
 		else if(checkValidKey(linkKey) < MAX_MEMORY_ENTRIES){
@@ -89,25 +79,6 @@ app.post('/myaction', function(req, res) {
 		}
 	}
 	
-	//DEBUG
-	console.log("Link: " + memoryLink[currMemory-1]);
-	console.log("Key: " + memoryKey[currMemory-1]);
-	console.log(currMemory);
-  //res.send('You sent the name "' + req.body.Url + '".');
 });
 
-
-/*
-http.createServer(function(request, response) {
-  var pathname = url.parse(request.url).pathname;
-  console.log("Request for " + pathname + " received.");
-  if(pathname === '/'){
-		response.redirect('./views/index.html');
-  }
-  
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(80);
-*/
 app.listen(80);
